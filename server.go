40632c0b977cff780ffbaf16164fa7f5113d5f24
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2024-05-28 18:30:33 -0700

    Update server.go

--- server.go
+++ server.go
@@ -3,7 +3,17 @@ package main
 import (
     "log"
     "net/http"
+    "path/filepath"
+    "time"
 )
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        start := time.Now()
+        next.ServeHTTP(w, r)
+        log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
+    })
+}
 
 func main() {
     fs := http.FileServer(http.Dir("./content"))
